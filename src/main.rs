3ca096cf3148362935c02c98f236c5a9e70c0064
James Smith <jismith1989@googlemail.com>
2026-09-17 16:15:43 +0100

    Add more arithmetic and logic opcodes

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,30 @@ use num_enum::TryFromPrimitive;
 
 #[derive(TryFromPrimitive)]
 #[repr(u8)]
-enum Opcode {
+enum OpcodeNibble {
 	Noop  = 0x0,
 	Push4 = 0x1,
 	Add   = 0x2,
+	Sub   = 0x3,
+	Mul   = 0x4,
 	Halt  = 0xE,
 	Ext   = 0xF,
 }
 
 #[derive(TryFromPrimitive)]
 #[repr(u8)]
-enum ExtendedOpcode {
-	Push8  = 0x0,
-	Push16 = 0x1,
-	Push32 = 0x2,
-	Push64 = 0x3,
+enum ExtendedOpcodeNibble {
+	Push8            = 0x0,
+	Push16           = 0x1,
+	Push32           = 0x2,
+	Push64           = 0x3,
+	LessThanUnsigned = 0x4,
+	LessThanSigned   = 0x5,
+	DivUnsigned      = 0x6,
+	DivSigned        = 0x7,
 }
+
+type Word = u64;
 
 fn get_next_nibble_from_program(program: &[u8], ip: &mut usize) -> u8 {
 	let byte = program[*ip / 2];
@@ -79,64 +87,121 @@ fn main() {
 	println!("Running: Language Substrate VM");
 
 	let example_program = [
-		0x1A,       // PUSH4 10
-		0xF0, 0x14, // PUSH8 20
-		0x2E,       // ADD, HALT
+		0x1A,                         // PUSH4  10
+		0xF0, 0x14,                   // PUSH8  20
+		0xF1, 0xE8, 0x03,             // PUSH16 1000
+		0xF2, 0x40, 0x42, 0x0F, 0x00, // PUSH32 1,000,000
+		0xE0,                         // HALT, padding
 	];
 
-	let mut stack: Vec<u64> = Vec::with_capacity(1 << 12);
+	let mut stack: Vec<Word> = Vec::with_capacity(1 << 12);
 	let mut ip: usize = 0; // instruction pointer, counts in nibbles
 
 	loop {
-		let raw_opcode = get_next_nibble_from_program(&example_program, &mut ip);
-		let opcode = Opcode::try_from(raw_opcode).expect("Unknown opcode");
+		let raw_opcode_nibble = get_next_nibble_from_program(&example_program, &mut ip);
+		let opcode_nibble = OpcodeNibble::try_from(raw_opcode_nibble).expect("Unknown opcode_nibble");
 
-		match opcode {
-			Opcode::Noop => {}
+		match opcode_nibble {
+			OpcodeNibble::Noop => {}
 
-			Opcode::Push4 => {
+			OpcodeNibble::Push4 => {
 				let value = get_next_nibble_from_program(&example_program, &mut ip);
-				stack.push(value as u64);
+				stack.push(value as Word);
 			}
 
-			Opcode::Add => {
+			OpcodeNibble::Add => {
 				let b = stack.pop().expect("Stack underflow");
 				let a = stack.pop().expect("Stack underflow");
 
 				stack.push(a + b);
 			}
 
-			Opcode::Halt => {
+			OpcodeNibble::Sub => {
+				let b = stack.pop().expect("Stack underflow");
+				let a = stack.pop().expect("Stack underflow");
+
+				stack.push(a.wrapping_sub(b));
+			}
+
+			OpcodeNibble::Mul => {
+				let b = stack.pop().expect("Stack underflow");
+				let a = stack.pop().expect("Stack underflow");
+
+				stack.push(a.wrapping_mul(b));
+			}
+
+			OpcodeNibble::Halt => {
 				break;
 			}
 
-			Opcode::Ext => {
+			OpcodeNibble::Ext => {
 				if ip % 2 == 0 {
-					panic!("Extended opcode is not byte-aligned");
+					panic!("Extended opcode_nibble is not byte-aligned");
 				}
 
-				let raw_extended_opcode = get_next_nibble_from_program(&example_program, &mut ip);
-				let extended_opcode = ExtendedOpcode::try_from(raw_extended_opcode).expect("Unknown extended opcode");
+				let raw_extended_opcode_nibble = get_next_nibble_from_program(&example_program, &mut ip);
+				let extended_opcode_nibble = ExtendedOpcodeNibble::try_from(raw_extended_opcode_nibble)
+					.expect("Unknown extended opcode_nibble");
 
-				match extended_opcode {
-					ExtendedOpcode::Push8 => {
+				match extended_opcode_nibble {
+					ExtendedOpcodeNibble::Push8 => {
 						let value = get_next_byte_from_program(&example_program, &mut ip);
-						stack.push(value as u64);
+						stack.push(value as Word);
 					}
 
-					ExtendedOpcode::Push16 => {
+					ExtendedOpcodeNibble::Push16 => {
 						let value = get_next_u16_from_program(&example_program, &mut ip);
-						stack.push(value as u64);
+						stack.push(value as Word);
 					}
 
-					ExtendedOpcode::Push32 => {
+					ExtendedOpcodeNibble::Push32 => {
 						let value = get_next_u32_from_program(&example_program, &mut ip);
-						stack.push(value as u64);
+						stack.push(value as Word);
 					}
 
-					ExtendedOpcode::Push64 => {
+					ExtendedOpcodeNibble::Push64 => {
 						let value = get_next_u64_from_program(&example_program, &mut ip);
-						stack.push(value);
+						stack.push(value as Word);
+					}
+
+					ExtendedOpcodeNibble::LessThanUnsigned => {
+						let b = stack.pop().expect("Stack underflow");
+						let a = stack.pop().expect("Stack underflow");
+
+						stack.push(if a < b { 1 } else { 0 });
+					}
+
+					ExtendedOpcodeNibble::LessThanSigned => {
+						let b = stack.pop().expect("Stack underflow") as i64;
+						let a = stack.pop().expect("Stack underflow") as i64;
+
+						stack.push(if a < b { 1 } else { 0 });
+					}
+
+					ExtendedOpcodeNibble::DivUnsigned => {
+						let b = stack.pop().expect("Stack underflow");
+						let a = stack.pop().expect("Stack underflow");
+
+						if b == 0 {
+							panic!("Division by zero");
+						}
+
+						stack.push(a / b);
+					}
+
+					ExtendedOpcodeNibble::DivSigned => {
+						let b = stack.pop().expect("Stack underflow") as i64;
+						let a = stack.pop().expect("Stack underflow") as i64;
+
+						if b == 0 {
+							panic!("Division by zero");
+						}
+
+						if a == i64::MIN && b == -1 {
+							stack.push(i64::MIN as Word);
+						} else {
+							stack.push((a / b) as Word);
+						}
 					}
 				}
 			}
