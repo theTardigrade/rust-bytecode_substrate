f4e584b1056df78440aba02c8ad1584c916fce00
James Smith <jismith1989@googlemail.com>
2026-09-17 14:30:07 +0100

    Intialize main loop and run example program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
 	println!("Running: Language Substrate VM");
+
+	let example_program =  [
+        1, 10, // PUSH 10
+        255,   // HALT
+    ];
+
+	let mut stack: Vec<i64> = Vec::new();
+	let mut ip: usize = 0; // instruction pointer
+
+	loop {
+		let opcode = example_program[ip];
+		ip += 1;
+
+		match opcode {
+			1 => {
+				let value = example_program[ip] as i64;
+				ip += 1;
+
+				stack.push(value);
+			}
+
+			255 => {
+				break;
+			}
+
+			_ => {
+				panic!("Unknown opcode: {}", opcode);
+			}
+		}
+	}
+
+	println!("Stack: {:?}", stack);
 }
